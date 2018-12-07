760c0339b18c4f82bc64e1dbfd666288e3ba48e7
KK <giorgioetto@mail.ru>
2018-12-07 09:30:59 +0500

    First commit

---
+++ JS site one/scripts/main.js
@@ -0,0 +1,26 @@
+'use strict';
+var myHeading = document.querySelector('h1');
+myHeading.textContent = 'Hello world!';
+// var leftborderWidth = 1;
+// let Rightstream = "Sensor";
+
+// console.log (Rightstream);
+//alert("What");
+//let answer = confirm("What are you doing?");
+//console.log(answer);
+// let answer = +prompt("What are you doing? Are you 18?", "Yes");
+// console.log(typeof(answer));
+ 
+// console.log("let" + "-object");
+// console.log(4 + "-object");
+let incr = 10,
+    decr = 10;
+
+console.log(incr++);
+console.log(decr--);
+console.log("2" === 2);
+
+let isChecked = true,
+    isClose = false;
+console.log(isChecked || isClose);
+
